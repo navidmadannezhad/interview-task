529f2cd2325c14feb741a99ca2e8908bef6eeb1b
navidmadannezhad <navidproject283@gmail.com>
2025-11-06 09:40:58 +0330

    fix: fix build error

--- src/utils/commonUtils.ts
+++ src/utils/commonUtils.ts
@@ -2,9 +2,10 @@ export const enToFaNum = (s: number | string): string | null => {
     if(s === undefined || s === null) return null;
 
     const number = typeof s === "number" ? String(s) : s; 
-    return number.replace(/\d/g, d => '۰۱۲۳۴۵۶۷۸۹'[d]);
+    // WIP -- solve any here!
+    return number.replace(/\d/g, d => '۰۱۲۳۴۵۶۷۸۹'[d as any]);
 }
-export const faToEnNum = (v: string): string => v ? v?.replace(/[۰-۹]/g, d => '۰۱۲۳۴۵۶۷۸۹'.indexOf(d)) : "";
+export const faToEnNum = (v: string): string => v ? v?.replace(/[۰-۹]/g, d => ('۰۱۲۳۴۵۶۷۸۹' as any).indexOf(d)) : "";
 
 export const humanizePrice = (price: number): string | null => {
     return enToFaNum(
