c820ad74bffc9198932770dafc67453ec6ac023c
navidmadannezhad <navidproject283@gmail.com>
2025-11-07 11:56:44 +0330

    fix: test build

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -2,14 +2,16 @@ import { PageWrapper, SectionWrapper } from "@/src/components/major";
 import { ProductDetail, ProductProperties } from "@/src/components/sections";
 import { getProductByID } from "@/src/services/api/ecommerce";
 
+export const dynamic = "force-dynamic";
+
 export default async function ProductDetailPage({ params }: any) {
-  // const { id } = await params;
-  // const { results } = await getProductByID({ id });
+  const { id } = await params;
+  const { results } = await getProductByID({ id });
 
   return (
     <PageWrapper>
       <></>
-      {/* <SectionWrapper>
+      <SectionWrapper>
         <ProductDetail
         //  WIP -- handle 404 not found here
           product={results}
@@ -19,7 +21,7 @@ export default async function ProductDetailPage({ params }: any) {
         <ProductProperties
           product={results}
         />
-      </SectionWrapper> */}
+      </SectionWrapper>
     </PageWrapper>
   );
 }
