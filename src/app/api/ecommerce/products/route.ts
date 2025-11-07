1c9f637079074152e13256673810a402c483848a
navidmadannezhad <navidproject283@gmail.com>
2025-11-07 18:26:05 +0330

    feat: handle pagination in apiRoute

--- src/app/api/ecommerce/products/route.ts
+++ src/app/api/ecommerce/products/route.ts
@@ -1,11 +1,18 @@
 import path from "path";
 import fs from "fs";
 import { ApiResponse, GetProductsResponseBodyDTO } from "@/src/models";
+import { getSearchParamsObject } from "@/src/utils/commonUtils";
 
 export async function GET(request: Request) {
+    const url = request.url;
+    const { offset, limit } = getSearchParamsObject(url);
+
     const filePath = path.join(process.cwd(), "public", "data", "products.json");
     const fileContent = fs.readFileSync(filePath, "utf-8");
-    const json = JSON.parse(fileContent);
+
+    const records = JSON.parse(fileContent);
+    const recordCount = records.length;
+    const json = records.slice(Number(offset), Number(limit) > recordCount ? recordCount : Number(limit));
 
     // WIP -- HANDLE possible ERRORS! and different erorr codes
 
