c27cb9da6a95b51f0ccb4f39ce5349d0d075ced8
navidmadannezhad <navidproject283@gmail.com>
2025-11-08 22:29:05 +0330

    fix: add source map

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  productionBrowserSourceMaps: true
 };
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
